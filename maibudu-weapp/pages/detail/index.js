9060fca9ca1bc06a652139ffbc25d4dc58e2375a
BuYan <thisisgpy@gmail.com>
2022-07-19 22:30:53 +0800

    完善书籍详情页

--- maibudu-weapp/pages/detail/index.js
+++ maibudu-weapp/pages/detail/index.js
@@ -1,15 +1,29 @@
 import invoke from '../../utils/http'
-
 Page({
 
     data: {
-
+        bookId: -1,
+        cover: '',
+        title: '',
+        subtitle: '',
+        author: '',
+        publisher: '',
+        publishDate: ''
     },
 
     async onLoad(options) {
+        const bookId = options.bookId
         const book = await invoke({
-            path: `/api/book/get?bookId=${options.bookId}`
+            path: `/api/book/get?bookId=${bookId}`
         })
-        console.log(book)
+        this.setData({
+            bookId: bookId,
+            cover: book.cover,
+            title: book.title,
+            subtitle: book.subtitle,
+            author: book.author,
+            publisher: book.publisher,
+            publishDate: book.publishDate
+        })
     }
 })
